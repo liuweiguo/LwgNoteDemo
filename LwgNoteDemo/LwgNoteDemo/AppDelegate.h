702153ecd0c64272aec71b9c15e9eae7c75409d4
liuewiguo <1145624927@qq.com>
2018-10-15 10:58:31 +0800

    创建项目

---
+++ LwgNoteDemo/LwgNoteDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LwgNoteDemo
+//
+//  Created by Macx on 2018/10/15.
+//  Copyright © 2018年 Macx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LwgNoteDemo/LwgNoteDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LwgNoteDemo
+//
+//  Created by Macx on 2018/10/15.
+//  Copyright © 2018年 Macx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
